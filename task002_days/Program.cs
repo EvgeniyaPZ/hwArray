9f71232e847c1df3a4c567576ce12c7cb5149f42
Jalini <jalini@mail.ru>
2022-11-06 02:08:46 +0300

    task002

---
+++ task002_days/Program.cs
@@ -0,0 +1,18 @@
+﻿bool weekDays (int Number)
+{
+    return (Number ==6 || Number ==7);
+}
+
+Console.WriteLine("Enter number: ");
+
+int A = Convert.ToInt32(Console.ReadLine());
+bool B = weekDays(A);
+
+if (B)
+{
+    Console.WriteLine("Weekend");
+}
+else
+{
+    Console.WriteLine("Working days");
+}
